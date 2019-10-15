4ecc06012334581f229e2c1d8ed1d065ee78bff5
jalmassi <justin.almassi@gmail.com>
2019-10-15 19:39:06 -0400

    Add files via upload

--- ultrasonic_obstacle_avoidance_experiment.ino
+++ ultrasonic_obstacle_avoidance_experiment.ino
@@ -1,6 +1,6 @@
-Justin Almassi
-Nicholas Bell
-Zixuan Chen 
+//Justin Almassi
+//Nicholas Bell
+//Zixuan Chen 
 
 
 //============================NICK==========================
@@ -18,14 +18,12 @@ int Trig =A0;
 int Echo1 = A3;
 int Trig1 = A2;
 
-int Echo2 = A5;
-int Trig2 = A4;
+int Echo2 = A4;
+int Trig2 = A5;
 
 int Distance = 0;
 int Distance1 = 0;
-int Distance2for = 0;
-int Distance2left = 0;
-int Distance2right = 0;
+int Distance2 = 0;
 //==============================
 //==============================
 int Left_motor_go=8;    
@@ -35,13 +33,11 @@ int Right_motor_go=6;
 int Right_motor_back=7;  
 
 int Right_motor_en=5;  
-int Left_motor_en=10;  
+int Left_motor_en=11;  
 
 /*Set Button port*/
 int key=13;
 
-/*Set BUZZER port*/
-int beep=12;
 
 
 void setup()
@@ -53,10 +49,10 @@ void setup()
   pinMode(Right_motor_go, OUTPUT); // PIN 6 (PWM)
   pinMode(Right_motor_back, OUTPUT); // PIN 7 (PWM)
   pinMode(Right_motor_en,OUTPUT);// PIN 5 (PWM)
-  pinMode(Left_motor_en,OUTPUT);// PIN 10 (PWM)
-  pinMode(key,INPUT);// Set button as input
-  pinMode(beep,OUTPUT);// Set buzzer as output
- 
+  pinMode(Left_motor_en,OUTPUT);// PIN 11 (PWM)
+  
+//  myservo.write(90);
+  
   pinMode(Echo, INPUT);    // Set Echo port mode
   pinMode(Trig, OUTPUT);   // Set Trig port mode
 
@@ -65,27 +61,28 @@ void setup()
 
   pinMode(Echo2, INPUT);    // Set Echo port mode
   pinMode(Trig2, OUTPUT);   // Set Trig port mode
- 
-  digitalWrite(key,HIGH);//Initialize button
-  digitalWrite(beep,LOW);// set buzzer mute
+// 
+//  digitalWrite(key,HIGH);//Initialize button
+  //digitalWrite(beep,LOW);// set buzzer mute
 
-  myservo.attach(11);
-  myservo.write(90);
+  
+  myservo.attach(12);
 }
-void run()     // ahead
+
+
+void run(int time)     // ahead
 {
- digitalWrite(Left_motor_en,HIGH);  // Left motor enable
-  analogWrite(Left_motor_en,90);
-  analogWrite(Right_motor_en,90);  // Right motor enable
-  digitalWrite(Right_motor_go,HIGH);  // right motor go ahead
-  digitalWrite(Right_motor_back,LOW);  
-  analogWrite(Right_motor_go,130);//PWM--Pulse Width Modulation(0~255). right motor go speed is 255.
-  analogWrite(Right_motor_back,0);
-  digitalWrite(Left_motor_go,HIGH);  // set left motor go ahead
-  digitalWrite(Left_motor_back,LOW);
+ analogWrite(Left_motor_en,100);
+  analogWrite(Right_motor_en,100);  // Right motor enable
+  //digitalWrite(Right_motor_go,HIGH);  // right motor go ahead
+  //digitalWrite(Right_motor_back,LOW);   
+  analogWrite(Right_motor_go,200);//PWM--Pulse Width Modulation(0~255). right motor go speed is 255.
+  analogWrite(Right_motor_back,0); 
+  //digitalWrite(Left_motor_go,HIGH);  // set left motor go ahead
+  //digitalWrite(Left_motor_back,LOW);
   analogWrite(Left_motor_go,137);//PWM--Pulse Width Modulation(0~255).left motor go speed is 135.
   analogWrite(Left_motor_back,0);
- // delay(time * 100);   //Running time can be adjusted
+  delay(time * 100); 
 }
 
 void brake(int time)         //STOP
@@ -99,57 +96,81 @@ void brake(int time)         //STOP
 
 void left(int time)        //turn left
 {
-  digitalWrite(Right_motor_go,HIGH); // right motor go ahead
-  digitalWrite(Right_motor_back,LOW);
-  analogWrite(Right_motor_go,190); // PWM--Pulse Width Modulation(0~255) control speed，right motor go speed is 255.
-  analogWrite(Right_motor_back,0);
-  digitalWrite(Left_motor_go,LOW);   // left motor stop
-  digitalWrite(Left_motor_back,LOW);
-  analogWrite(Left_motor_go,0);
-  analogWrite(Left_motor_back,0);
-  delay(time * 100);
+  analogWrite(Left_motor_en,100);
+  analogWrite(Right_motor_en,120);  // Right motor enable
+  digitalWrite(Right_motor_go,HIGH);  // right motor go ahead
+  digitalWrite(Right_motor_back,LOW);   
+  //analogWrite(Right_motor_go,200);//PWM--Pulse Width Modulation(0~255). right motor go speed is 255.
+  //analogWrite(Right_motor_back,0); 
+  //digitalWrite(Left_motor_go,HIGH);  // set left motor go ahead
+  //digitalWrite(Left_motor_back,LOW);
+  //analogWrite(Left_motor_go,137);//PWM--Pulse Width Modulation(0~255).left motor go speed is 135.
+  //analogWrite(Left_motor_back,0);
+  delay(time * 100); 
 }
 void spin_left(int time)   //Left rotation
 {
-  digitalWrite(Right_motor_go,HIGH);// right motor go ahead
-  digitalWrite(Right_motor_back,LOW);
-  analogWrite(Right_motor_go,200);// PWM--Pulse Width Modulation(0~255) control speed ,right motor go speed is 200.
-  analogWrite(Right_motor_back,0);
-  digitalWrite(Left_motor_go,LOW);   // left motor back off
-  digitalWrite(Left_motor_back,HIGH);
-  analogWrite(Left_motor_go,0);
-  analogWrite(Left_motor_back,200);// PWM--Pulse Width Modulation(0~255) control speed,left motor back speed is 200.
+  analogWrite(Left_motor_en,150);
+  analogWrite(Right_motor_en,150);  // Right motor enable
+  //digitalWrite(Right_motor_go,HIGH);  // right motor go ahead
+  //digitalWrite(Right_motor_back,LOW);   
+  analogWrite(Right_motor_go,255);//PWM--Pulse Width Modulation(0~255). right motor go speed is 255.
+  analogWrite(Right_motor_back,0); 
+  //digitalWrite(Left_motor_go,HIGH);  // set left motor go ahead
+  //digitalWrite(Left_motor_back,LOW);
+  analogWrite(Left_motor_go,0);//PWM--Pulse Width Modulation(0~255).left motor go speed is 135.
+  analogWrite(Left_motor_back,255);
   delay(time * 100);
 }
 
 void right(int time)      //turn right
 {
- digitalWrite(Right_motor_go,LOW);   // right motor stop
-  digitalWrite(Right_motor_back,LOW);
-  analogWrite(Right_motor_go,0);
-  analogWrite(Right_motor_back,0);
-  digitalWrite(Left_motor_go,HIGH);// left motor go ahead
-  digitalWrite(Left_motor_back,LOW);
-  analogWrite(Left_motor_go,230);// PWM--Pulse Width Modulation(0~255) control speed ,left motor go speed is 255.
+ analogWrite(Left_motor_en,100);
+  analogWrite(Right_motor_en,100);  // Right motor enable
+  //digitalWrite(Right_motor_go,HIGH);  // right motor go ahead
+  //digitalWrite(Right_motor_back,LOW);   
+  //analogWrite(Right_motor_go,200);//PWM--Pulse Width Modulation(0~255). right motor go speed is 255.
+  //analogWrite(Right_motor_back,0); 
+  //digitalWrite(Left_motor_go,HIGH);  // set left motor go ahead
+  //digitalWrite(Left_motor_back,LOW);
+  analogWrite(Left_motor_go,137);//PWM--Pulse Width Modulation(0~255).left motor go speed is 135.
   analogWrite(Left_motor_back,0);
-  delay(time * 100);
+  delay(time * 100); 
 }
 
 void spin_right(int time)   //Right rotation
 {
-  digitalWrite(Right_motor_go,LOW);  // right motor back off
-  digitalWrite(Right_motor_back,HIGH);
-  analogWrite(Right_motor_go,0);
-  analogWrite(Right_motor_back,200);// PWM--Pulse Width Modulation(0~255) control speed
-  digitalWrite(Left_motor_go,HIGH);// left motor go ahead
-  digitalWrite(Left_motor_back,LOW);
-  analogWrite(Left_motor_go,200);// PWM--Pulse Width Modulation(0~255) control speed
+analogWrite(Left_motor_en,150);
+  analogWrite(Right_motor_en,150);  // Right motor enable
+  //digitalWrite(Right_motor_go,HIGH);  // right motor go ahead
+  //digitalWrite(Right_motor_back,LOW);   
+  analogWrite(Right_motor_go,0);//PWM--Pulse Width Modulation(0~255). right motor go speed is 255.
+  analogWrite(Right_motor_back,255); 
+  //digitalWrite(Left_motor_go,HIGH);  // set left motor go ahead
+  //digitalWrite(Left_motor_back,LOW);
+  analogWrite(Left_motor_go,255);//PWM--Pulse Width Modulation(0~255).left motor go speed is 135.
   analogWrite(Left_motor_back,0);
-  delay(time * 100);
+  delay(time * 100); 
 }
 
 void back(int time)   //back off
 {
+  analogWrite(Left_motor_en,100);
+  analogWrite(Right_motor_en,100);  // Right motor enable
+  //digitalWrite(Right_motor_go,HIGH);  // right motor go ahead
+  //digitalWrite(Right_motor_back,LOW);   
+  analogWrite(Right_motor_go,0);//PWM--Pulse Width Modulation(0~255). right motor go speed is 255.
+  analogWrite(Right_motor_back,255); 
+  //digitalWrite(Left_motor_go,HIGH);  // set left motor go ahead
+  //digitalWrite(Left_motor_back,LOW);
+  analogWrite(Left_motor_go,0);//PWM--Pulse Width Modulation(0~255).left motor go speed is 135.
+  analogWrite(Left_motor_back,255);
+  delay(time * 100); 
+}
+
+//==========================================================
+
+void spin(int time){
   digitalWrite(Right_motor_go,LOW); //right motor back off
   digitalWrite(Right_motor_back,HIGH);
   analogWrite(Right_motor_go,0);
@@ -157,13 +178,11 @@ void back(int time)   //back off
   analogWrite(Right_motor_en,165);
   digitalWrite(Left_motor_go,LOW);  //left motor back off
   digitalWrite(Left_motor_back,HIGH);
-  analogWrite(Left_motor_go,0);
-  analogWrite(Left_motor_back,140);// PWM--Pulse Width Modulation(0~255) control speed
+  analogWrite(Left_motor_go,200);
+  analogWrite(Left_motor_back,0);// PWM--Pulse Width Modulation(0~255) control speed
+  analogWrite(Left_motor_en,165);
   delay(time * 100);
 }
-
-//==========================================================
-
 
 
 
@@ -212,7 +231,7 @@ int Distance_test2()   // Measuring front distance
                                  // ==> Distance(cm) = time(μs) * 0.0172 = time(μs) / 58
   Serial.print("Distance2:");      //Output Distance(cm)
   Serial.println(Fdistance);         //display distance
-  return Fdistance;
+  Distance2 = Fdistance;
 }  
 //==========================================================
 
@@ -231,13 +250,13 @@ void keysacn()
     if(val==LOW)  //Double check the button is pressed
     {
        
-      digitalWrite(beep,HIGH);//The buzzer sounds
+      //digitalWrite(beep,HIGH);//The buzzer sounds
       delay(50);//delay 50ms
       while(!digitalRead(key)) //Determine if the button is released or not
-        digitalWrite(beep,LOW);//mute
+        ;//digitalWrite(beep,LOW);//mute
     }
     else
-      digitalWrite(beep,LOW);//mute
+      ;//digitalWrite(beep,LOW);//mute
   }
 }
 /*main loop*/
@@ -245,67 +264,202 @@ void keysacn()
 //Servo Look
 //================================
 
-void servoLookfor() {
+void servoLookForward() {
   myservo.write(90);
-  delay(300)
-  Distance2for = Distance_test2();
+  delay(500);
   delay(10);
 }
 void servoLookRight() {
   myservo.write(135);
-  delay(300)
-  Distance2right = Distance_test2();
+  delay(500);
   delay(10);
 }
-void servoLookleft() {
+void servoLookLeft() {
   myservo.write(45);
-  delay(600);
-  Distance2for = Distance_test2();
+  delay(500);
   delay(10);
 }
 void lookDown() {
   Distance_test();//Measuring front distance
-  Distance_test1();
-  delay(10);
-}
+  Distance_test2();
+  }
 void response() {
-  if(Distance > 30|| Distance1 > 30)//The value is the distance that meets the obstacle, and can be set according to the actual situation  
+  if(Distance > 30|| Distance1 > 30 || Distance2 < 30)//The value is the distance that meets the obstacle, and can be set according to the actual situation  
     {
-      delay(10);
       Distance_test();//Measuring front distance
       Distance_test1();
-      while(Distance > 30 || Distance1 > 30)//Determine whether there is an obstruction again.If there is obstacle , turn the direction and determine again.
+      Distance_test2();
+      while(Distance > 30 || Distance1 > 30 || Distance2 < 30)//Determine whether there is an obstruction again.If there is obstacle , turn the direction and determine again.
       {
        back(1);  
        spin_right(1);//Right rotation for 300ms
        brake(1);//stop
-       delay(100);
+       
        Distance_test();//Measuring front distance
-       Distance_test1();
-       delay(10);
+       Distance_test2();
+       
          
       }
     }
     else
-         run();//There is nothing obstacled. Go ahead.
+         run(1);//There is nothing obstacled. Go ahead.
 }
 
+
+void response1()
+{  
+    Distance_test1();
+    lookDown();
+    if(Distance >15 || Distance2 >15)//The value is the distance that meets the obstacle, and can be set according to the actual situation   
+    { 
+      Distance_test1();
+      lookDown();
+      while(Distance>15 || Distance2 > 15)//Determine whether there is an obstruction again.If there is obstacle , turn the direction and determine again.
+      {
+       brake(1);//stop
+       spin_left(5);//Right rotation for 300ms
+       brake(1);
+        Distance_test1();
+        lookDown();
+      }
+    }
+    else 
+         run(1);//There is nothing obstacled. Go ahead.
+       
+  }
+
+void response2()
+{  
+    Distance_test1();
+    lookDown();
+    if(Distance1 < 15)//The value is the distance that meets the obstacle, and can be set according to the actual situation   
+    { 
+      Distance_test1();
+      lookDown();
+      while(Distance1 < 15)//Determine whether there is an obstruction again.If there is obstacle , turn the direction and determine again.
+      {
+       brake(1);//stop
+       spin_left(3);
+       brake(1);
+        Distance_test1();
+        lookDown();
+      }
+    }
+    else 
+         run(1);//There is nothing obstacled. Go ahead.
+       
+  }
+
+ void response3()
+{  
+    Distance_test1();
+    if(Distance1 < 30)//The value is the distance that meets the obstacle, and can be set according to the actual situation   
+    { 
+      Distance_test1();
+      lookDown();
+      while(Distance1 < 30)//Determine whether there is an obstruction again.If there is obstacle , turn the direction and determine again.
+      {
+       brake(1);//stop
+       spin_left(3);//Right rotation for 300ms
+       brake(1);
+        Distance_test1();
+        lookDown();
+      }
+    }
+    
+       
+  }
+
+void response4()
+{  
+    Distance_test1();
+    if(Distance1 < 30)//The value is the distance that meets the obstacle, and can be set according to the actual situation   
+    { 
+      Distance_test1();
+      lookDown();
+      while(Distance1 < 30)//Determine whether there is an obstruction again.If there is obstacle , turn the direction and determine again.
+      {
+       brake(1);//stop
+       spin_right(10);//Right rotation for 300ms
+       brake(1);
+        Distance_test1();
+        lookDown();
+      }
+    }
+    
+       
+  }
+void response5()
+{  
+    Distance_test1();
+    if(Distance1 < 30)//The value is the distance that meets the obstacle, and can be set according to the actual situation   
+    { 
+      Distance_test1();
+      lookDown();
+      while(Distance1 < 30)//Determine whether there is an obstruction again.If there is obstacle , turn the direction and determine again.
+      {
+        while(Distance1 < 10){
+          brake(1);//stop
+       back(1);//Right rotation for 300ms
+       brake(1);
+        Distance_test1();
+        lookDown();
+        }
+       brake(1);//stop
+       spin_left(3);//Right rotation for 300ms
+       brake(1);
+        Distance_test1();
+        lookDown();
+      }
+    }
+    
+       
+  }
+
+
+void responseL()
+{  
+    Distance_test1();
+    lookDown();
+    if(Distance2 >15)//The value is the distance that meets the obstacle, and can be set according to the actual situation   
+    { 
+      Distance_test1();
+      lookDown();
+      while(Distance2 > 15)//Determine whether there is an obstruction again.If there is obstacle , turn the direction and determine again.
+      {
+       brake(1);//stop
+       spin_right(1);//Right rotation for 300ms
+       brake(1);
+        Distance_test1();
+        lookDown();
+      }
+    }
+    else 
+         run(1);//There is nothing obstacled. Go ahead.
+       
+  }
+ 
 void loop()
 {
   keysacn();//Press the button to start
  
   while(1)
   {
-    servoLookfor();
-    lookDown();
-    response();
-   
+
+    response1();
+
+    brake(1);
     servoLookRight();
-    lookDown();
-    response();
+    response5();
+    
+    
+    servoLookForward();
+    response3();
+    
+    servoLookLeft();
+    response4();
+    
    
-    servoLookleft();
-    lookDown();
-    response();
+
   }
 }
